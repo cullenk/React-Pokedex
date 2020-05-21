5c6dce050bc97dcd7cbfac2c0ab0b9571b8cee2c
cullenk <45953462+cullenk@users.noreply.github.com>
2020-05-21 13:34:50 -0400

    Updates

---
+++ precache-manifest.26d4de8530bba6ba369af0624624daab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0220eaeb06f6d501171e4f39401a24b",
+    "url": "/React-Pokedex/index.html"
+  },
+  {
+    "revision": "3902a52681ef7a202f81",
+    "url": "/React-Pokedex/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "0fd7af1051ca1c8d88a6",
+    "url": "/React-Pokedex/static/js/2.62b51851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Pokedex/static/js/2.62b51851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3902a52681ef7a202f81",
+    "url": "/React-Pokedex/static/js/main.c52f8497.chunk.js"
+  },
+  {
+    "revision": "6a17b84e978451a68dc9",
+    "url": "/React-Pokedex/static/js/runtime-main.af62f92a.js"
+  }
+]);
